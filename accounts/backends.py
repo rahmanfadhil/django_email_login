77f43cf1756858df5021d657a1f1d1fe93aa1d36
Tim Schneider <tim_schneider@intention.de>
2021-05-05 22:49:55 +0200

    Fixing code branc for multiple Objects returned
    - Fixing Exception Class
    - Fixing filter parameters to match the ones on line 13.
    - refactoring code, so thet the password of the user returned in the `MultipleObjectsReturned` branch are also verified

--- accounts/backends.py
+++ accounts/backends.py
@@ -13,11 +13,12 @@ class EmailBackend(ModelBackend):
                 Q(username__iexact=username) | Q(email__iexact=username))
         except UserModel.DoesNotExist:
             UserModel().set_password(password)
-        except MultipleObjectsReturned:
-            return User.objects.filter(email=username).order_by('id').first()
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
+            return
+        except UserModel.MultipleObjectsReturned:
+            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+        
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
 
     def get_user(self, user_id):
         try:
